a1a4d6af31e3cd82d8b66b2b3f467c7c61c66012
JohnLcatz <138212695+JohnLcatz@users.noreply.github.com>
2024-05-20 22:32:25 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("navigateButton");
+
+    button.addEventListener("click", function() {
+        window.location.href = "navigation.html";
+    });
+});
